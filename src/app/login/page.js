c1233d049b16a64afdcc01b67a465fe6bb2d9c31
Cifpb <ana.costa.2@academico.ifpb.edu.br>
2024-12-10 10:41:42 -0300

    login arrumou css

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,9 +5,8 @@ import Footer from "../footer/footer";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import Limg from "../../../public/login/img.png";
 import Image from "next/image";
-import './login.css';
+import style from './page.module.css';
 import Link from 'next/link';
-
 
 export default function Login() {
 
@@ -25,21 +24,27 @@ export default function Login() {
   };
 
   return (
-    <div className="logando">
+    <div className={style.logando}>
       <Header />
-      <div className="pag-login">
-        <div className="elem-login">
-          <div className="imagem-login">
-           <Image src={Limg} alt= "imagem-login"/>
+      <div className={style.pag_login}>
+        <div className={style.elem_login}>
+          <div className={style.imagem_login}>
+            <Image src={Limg} alt="imagem-login"/>
           </div>
-          <div className="form-login">
-            <h2 className="texto-principal">LOGIN</h2>
+          <div className={style.form_login}>
+            <h2 className={style.texto_principal}>LOGIN</h2>
             <form action="#aqui usa o backend" method="post">
-              <div className="formulario-grupo">
+              <div className={style.formulario_grupo}>
                 <label htmlFor="email"></label>
-                <input type="email" id="email" placeholder="E-mail" required autoComplete="off" />
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="E-mail"
+                  required
+                  autoComplete="off"
+                />
               </div>
-              <div className="formulario-grupo">
+              <div className={style.formulario_grupo}>
                 <label htmlFor="password"></label>
                 <input
                   type={mostrarSenha ? "text" : "password"} // Usando o estado mostrarSenha para alternar entre "text" e "password"
@@ -49,18 +54,22 @@ export default function Login() {
                   placeholder="Senha"
                   required
                 />
-                <button type="button" className="icone-eye" onClick={autMostrarSenha}>
+                <button
+                  type="button"
+                  className={style.icone_eye}
+                  onClick={autMostrarSenha}
+                >
                   {mostrarSenha ? <FaRegEyeSlash /> : <FaRegEye />}
                 </button>
               </div>
-              <div className="formulario-links">
+              <div className={style.formulario_links}>
                 <Link href="/redefinir-senha"> Esqueceu a senha? </Link>
-                <div className="formulario-grupo">
+                <div className={style.formulario_grupo}>
                   <center>
                     <input type="submit" defaultValue="Entrar" />
                   </center>
                 </div>
-                <span className="texto-criar-conta">Ainda não tem conta?</span>
+                <span className={style.texto_criar_conta}>Ainda não tem conta?</span>
                 <Link href="/cadastrar/dados-pessoais"> Cadastre-se </Link>
               </div>
             </form>
